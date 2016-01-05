8c9d3c83fa92cd807ff7cdd6a4325cd14bee75b9
brandon s allbery kf8nh <allbery.b@gmail.com>
2016-01-05 16:27:31 -0500

    fix some perms; clean up links.pl a bit
    including a reversed test in cleanup code

--- links.pl
+++ links.pl
@@ -1,18 +1,26 @@
 #! /usr/bin/perl
+use strict;
+use warnings;
 # yes, this is disgustingly quick and dirty
 chomp(my $dir = `pwd`);
+if (substr($dir, 0, length($ENV{HOME}) + 1) eq "$ENV{HOME}/") {
+  substr($dir, 0, length($ENV{HOME}) + 1, '');
+}
 $dir .= '/';
+print "repo = $dir\nhome = $ENV{HOME}\n";
 my ($d, $f);
+print "scan for additions\n";
 opendir $d, '.' or die "where am i?";
 FILE:
-while ($f = readdir $d) {
+while (defined ($f = readdir $d)) {
   for (qw(. .. .git .gitignore links.pl LICENSE README.md)) {
     next FILE if $_ eq $f;
   }
-  next if $f =~ /^\.#/ || $f =~ /(~|\.swp)$/; # editor temp files
+  next FILE if $f =~ /^\.#/ || $f =~ /(~|\.swp)$/; # editor temp files
   if (! -e "$ENV{HOME}/$f") {
     if (@ARGV) {
-      warn "eventually symlink $f\n";
+      warn "symlink $f\n";
+      symlink "$dir/$f", "$ENV{HOME}/$f";
     } else {
       print "would symlink $f\n";
     }
@@ -20,19 +28,33 @@ while ($f = readdir $d) {
 }
 closedir $d;
 my $l;
+print "scan for removals\n";
 opendir $d, $ENV{HOME} or die "where are you?";
-while ($f = readdir $d) {
-  next if $f eq '.' or $f eq '..';
-  if (defined ($l = readlink("$ENV{HOME}/$f"))) {
-    next unless substr($l, 0, length $dir) eq $dir;
-    next unless -e $f;
+OLD:
+while (defined ($f = readdir $d)) {
+  next OLD if $f eq '.' or $f eq '..';
+  if (defined ($l = readlink("$ENV{HOME}/$f")) &&
+      substr($l, 0, length($dir)) eq $dir) {
+    next OLD if -e $f;
     if (@ARGV) {
-      warn "eventually remove $f symlink\n";
+      warn "remove $f symlink\n";
+      unlink "$ENV{HOME}/$f";
     } else {
       print "would unlink $f\n";
     }
   }
   elsif (-e $f) {
-    print "managed $f is not a symlink\n";
+    if (@ARGV == 1 && $ARGV[0] eq 'FORCE') {
+      (my $bf = $f) =~ s/^\.//;
+      $f .= ".orig";
+      if (-e "$ENV{HOME}/$bf") {
+	die "can't back up $f to $bf";
+      }
+      warn "managing $f; backup at $ENV{HOME}/$bf";
+      rename "$ENV{HOME}/$f", "$ENV{HOME}/$bf";
+      symlink "$dir/$f", "$ENV{HOME}/$f";
+    } else {
+      print "$f is unmanaged\n";
+    }
   }
 }
